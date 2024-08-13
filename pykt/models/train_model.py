d2bda79e535178ec0b16e139c96a14dbabc53dae
kejinsen <kejinsen@gmail.com>
2024-08-13 12:47:39 +0800

    First commit

---
+++ pykt/models/train_model.py
@@ -0,0 +1,90 @@
+import os, sys
+import torch
+import torch.nn as nn
+from torch.nn.functional import one_hot, binary_cross_entropy, cross_entropy
+from torch.nn.utils.clip_grad import clip_grad_norm_
+import numpy as np
+from .evaluate_model import evaluate
+from torch.autograd import Variable, grad
+from ..utils.utils import debug_print
+from pykt.config import que_type_models
+import pandas as pd
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def cal_loss(model, ys, r, rshft, sm, preloss=[]):
+    model_name = model.model_name
+
+    if model_name in ["fluckt"]:
+        y = torch.masked_select(ys[0], sm)
+        t = torch.masked_select(rshft, sm)
+        loss = binary_cross_entropy(y.double(), t.double()) + preloss[0]
+    return loss
+
+
+def model_forward(model, data, rel=None):
+    model_name = model.model_name
+    # if model_name in ["dkt_forget", "lpkt"]:
+    #     q, c, r, qshft, cshft, rshft, m, sm, d, dshft = data
+    dcur = data
+
+    q, c, r, t = dcur["qseqs"].to(device), dcur["cseqs"].to(device), dcur["rseqs"].to(device), dcur["tseqs"].to(device)
+    qshft, cshft, rshft, tshft = dcur["shft_qseqs"].to(device), dcur["shft_cseqs"].to(device), dcur["shft_rseqs"].to(device), dcur["shft_tseqs"].to(device)
+    m, sm = dcur["masks"].to(device), dcur["smasks"].to(device)
+
+    ys, preloss = [], []
+    cq = torch.cat((q[:,0:1], qshft), dim=1)
+    cc = torch.cat((c[:,0:1], cshft), dim=1)
+    cr = torch.cat((r[:,0:1], rshft), dim=1)
+    
+    if model_name in ["fluckt"]:               
+        y, reg_loss = model(cc.long(), cr.long(), cq.long())
+        ys.append(y[:,1:])
+        preloss.append(reg_loss)
+
+    loss = cal_loss(model, ys, r, rshft, sm, preloss)
+    return loss
+    
+
+def train_model(model, train_loader, valid_loader, num_epochs, opt, ckpt_path, test_loader=None, test_window_loader=None, save_model=False, data_config=None, fold=None):
+    max_auc, best_epoch = 0, -1
+    train_step = 0
+
+    rel = None
+
+    for i in range(1, num_epochs + 1):
+        loss_mean = []
+        for data in train_loader:
+            train_step+=1
+            model.train()
+            loss = model_forward(model, data)
+            opt.zero_grad()
+            loss.backward()#compute gradients
+            opt.step()#update model’s parameters
+                
+            loss_mean.append(loss.detach().cpu().numpy())
+        loss_mean = np.mean(loss_mean)
+        
+        auc, acc = evaluate(model, valid_loader, model.model_name)
+        if auc > max_auc+1e-3:
+            if save_model:
+                torch.save(model.state_dict(), os.path.join(ckpt_path, model.emb_type+"_model.ckpt"))
+            max_auc = auc
+            best_epoch = i
+            testauc, testacc = -1, -1
+            window_testauc, window_testacc = -1, -1
+            if not save_model:
+                if test_loader != None:
+                    save_test_path = os.path.join(ckpt_path, model.emb_type+"_test_predictions.txt")
+                    testauc, testacc = evaluate(model, test_loader, model.model_name, save_test_path)
+                if test_window_loader != None:
+                    save_test_path = os.path.join(ckpt_path, model.emb_type+"_test_window_predictions.txt")
+                    window_testauc, window_testacc = evaluate(model, test_window_loader, model.model_name, save_test_path)
+            validauc, validacc = auc, acc
+        print(f"Epoch: {i}, validauc: {validauc:.4}, validacc: {validacc:.4}, best epoch: {best_epoch}, best auc: {max_auc:.4}, train loss: {loss_mean}, emb_type: {model.emb_type}, model: {model.model_name}, save_dir: {ckpt_path}")
+        print(f"            testauc: {round(testauc,4)}, testacc: {round(testacc,4)}, window_testauc: {round(window_testauc,4)}, window_testacc: {round(window_testacc,4)}")
+
+
+        if i - best_epoch >= 10:
+            break
+    return testauc, testacc, window_testauc, window_testacc, validauc, validacc, best_epoch
